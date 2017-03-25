929848ba535f550adb24f17c37bc71c8e0575a84
anont34 <xgrits@gmail.com>
2017-03-25 08:51:49 +0200

    The search is viable

--- Travel Office.cpp
+++ Travel Office.cpp
@@ -33,33 +33,34 @@ int main()
 	char *countryname = new char[20];
 	do
 	{
-	cout << "Country Tour Search\n" << "Enter country: "; // not work
+	cout << "Country Tour Search\n" << "Enter country: ";
 	cin >> countryname; cout << endl;
 
-	if(countryname == Base.country)
+	if(strcmp(countryname,Base.country) == 0)
 	{
-		cout << "Value = " << Base.value
-		<< "Rest days " << Base.days
-		<< "When end = " << Base.dayend << endl;
+		cout << "Value: " << Base.value
+		<< "\nRest days: " << Base.days
+		<< "\nWhen end: " << Base.dayend << endl;
 		break;
 	}
-	if(countryname == Medium.country)
+	if(strcmp(countryname,Medium.country) == 0)
 	{
-		cout << "Value = " << Medium.value
-		<< "Rest days " << Medium.days
-		<< "When end = " << Medium.dayend << endl;
+		cout << "Value: " << Medium.value
+		<< "\nRest days: " << Medium.days
+		<< "\nWhen end: " << Medium.dayend << endl;
 		break;
 	}
-	if(countryname == Hard.country)
+	if(strcmp(countryname,Hard.country) == 0)
 	{
-		cout << "Value = " << Hard.value
-		<< "Rest days " << Hard.days
-		<< "When end = " << Hard.dayend << endl;
+		cout << "Value: " << Hard.value
+		<< "\nRest days: " << Hard.days
+		<< "\nWhen end: " << Hard.dayend << endl;
 		break;
 	}
 	else
 		cout << "Error. Not fornd this country. Try again." << endl;
 	}while(true);
+
 system("pause");
 return 0;
 }
